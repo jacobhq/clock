3f5483473a7dd688272faeeeda4f3898ac50b139
jacobhq <jacob.marshall.jem@gmail.com>
2021-01-07 22:41:09 +0000

    Stop work

---
+++ components/welcome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <section id="welcome">
+      <div class="center">
+        <p class="tag">👋 Hello there, and</p>
+        <h1 class="welcome">Welcome</h1>
+        <br>
+        <div style="animation: 0.3s appear;">
+          <a href="#words" class="btn">Begin</a>
+          <a href="#" class="btn n-bg">↑</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  75% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
